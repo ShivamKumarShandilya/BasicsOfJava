022fd0a74d72a1f87385e98929bfd7de8219bafb
Shivam Sharma <Shivam Sharma@MSI>
2022-04-02 21:46:11 +0530

    hi

---
+++ BasicsOfJava/src/unit01/p2_Task2_KeywordsInJava.java
@@ -0,0 +1,18 @@
+package unit01;
+
+public class p2_Task2_KeywordsInJava {
+	
+
+	protected static final double PRICE =234.90;
+	public static final double PRICE2 = 234.90;
+	
+	static final int a = 10;
+	
+	public static void main(String[] args) {
+		
+		System.out.println(PRICE);
+		
+		System.out.println(a);
+
+	}
+}
