fd3aed9484610ce33ea42314480963b4fe913cad
Shivam Sharma <Shivam Sharma@MSI>
2022-04-10 21:08:34 +0530

    String

--- BasicsOfJava/src/unit01/p8_string.java
+++ BasicsOfJava/src/unit01/p8_string.java
@@ -9,7 +9,7 @@ public class p8_string {
 		String s2 = new String("7545a");
 
 		obj.palindromeOrNot(s1);
-		obj.reverseOfAString(s1);
+		obj.reverseOfAString(s2);
 		obj.stringEqualOrNot(s1, s2);
 
 	}
@@ -19,24 +19,48 @@ public class p8_string {
 class QuestionsOnString {
 
 	void palindromeOrNot(String s) {
+		int j = s.length()-1;
+		Boolean b = true;
+		for(int i = 0 ;i<=j; i++)
+		{
+			if(s.charAt(i)!= s.charAt(j))
+			{
+				b = false;
+				break;
+				
+			}j--;
+		}
+		if(b) {
+			System.out.println("string is pallindrome");
+		}
+		System.out.println("not pallindrome");
+		
+		
+		
 		
 
 	}
 
 	void reverseOfAString(String s) {
-		string a1 = "";
-		for(int i=0;i<s.lenght();i++)
+		String st1=" ";
+		for(int i =0 ; i<s.length() ;i++)
 		{
 			char ch = s.charAt(i);
-			a1 = ch + a1;
- 		}
-		System.out.println("Reverse string is; ");
-		
+			st1 = ch+st1;
+			
+		}System.out.println("reverse "+st1);
 	}
 	
+	
 	void stringEqualOrNot(String s1,String s2) {
-		
-		
+		if(s1.equals(s2))
+		{
+			System.out.println("s1 = s2 ");
+		}
+		else {
+			System.out.println("s1 not equal to s2");
+		}
 	}
+		
 }
 
